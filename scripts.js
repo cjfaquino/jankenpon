b14588c344b5b089382a71d49ec7d47701c82fda
Christian Aquino <cjfaquino@gmail.com>
2022-03-20 04:21:36 -0700

    add playRound()

--- scripts.js
+++ scripts.js
@@ -1,21 +1,38 @@
-// ask player for selection
+    // ask player for selection
 
-// lowercase player input for uniformity
+    // store player selection
 
-// store player selection
+function playRound(playerSelection, computerSelection) {
+    //make playerSelection uniform
+    let pSelect = playerSelection.toLowerCase();
+    pSelect = pSelect[0].toUpperCase()+pSelect.slice(1)
+    
+    // compare computer and player selction
+    // select winner or draw
+    if(pSelect === computerSelection ) {
+        return "It's a draw!"
+    } else if (pSelect === 'Rock' && computerSelection === 'Paper'
+            || pSelect === 'Scissors' && computerSelection === 'Rock'
+            || pSelect === 'Paper' && computerPlay === 'Scissors') {
+                return `You Lose! ${computerSelection} beats ${pSelect.toLowerCase()}.`
+    } 
+    return `You win! ${pSelect} beats ${computerSelection.toLowerCase()}.`
+}
+
+const playerSelection = 'Rock'
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
 
 // generate computer selection randomly
 function computerPlay() {
-    let choices = ['Rock', 'Papers', 'Scissors']
+    let choices = ['Rock', 'Paper', 'Scissors']
 
     //generate random number up to arrayLength
     const pick = Math.floor(Math.random()*choices.length)
-    
+
     return choices[pick]
 }
 
 
-// compare computer and player selction
 
-// select winner or draw
 
